4423e407109730b2f7b84e177ace28b805e0ec0a
Jul <yulya.ilina.2014@bk.ru>
2020-01-13 14:19:06 +0300

    Fixed CombSort

---
+++ Algorithms/Sorts/InsertionSort.py
@@ -0,0 +1,19 @@
+lst = [-7, 6, 1, 15, 11, 8, 4, 1, 10, -2]
+
+
+def insertion(array):
+    sort = array[:]
+    for i, elem in enumerate(sort):
+        j = i - 1
+        while elem < sort[j] and j >= 0:
+            j -= 1
+
+        if j != i - 1:
+            sort.insert(j + 1, elem)
+            sort.pop(i + 1)
+            print(sort)
+    return sort
+
+
+print(insertion(lst))
+
