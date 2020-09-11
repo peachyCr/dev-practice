2e12b9f2e09d76d61c197a80f64cd1c632230735
peachyCr <yulya.ilina.2014@bk.ru>
2020-09-11 14:15:09 +0300

    299

---
+++ LeetCode/C++/Easy/299 Bulls and Cows.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    string getHint(string secret, string guess)
+    {
+        int bulls = 0; int cows = 0; int size = secret.size();
+        for (int i = 0; i < guess.size(); i++)
+        {
+            if (secret[i] == guess[i])
+            {
+                bulls++;
+                secret[i] = 'i';
+                guess[i] = 'a';
+            }
+        }
+        for (int i = 0; i < guess.size(); i++)
+        {
+            int index = secret.find(guess[i]);
+            if (index != -1)
+            {
+                cows++;
+                secret[index] = 'i';
+            }
+        }
+        string res = to_string(bulls) + 'A' + to_string(cows) + 'B';
+        return res;
+    }
+};
